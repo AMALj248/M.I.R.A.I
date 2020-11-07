Fixed till Model Input, Scaling still left

Look_Back_Maker.py
@@ -7,12 +7,12 @@ def look_back(data, look_back_window) :
     # selecting from i to i+look_back with 1 left
     print("IN\n", data.shape)
     for i in range(len(data)-look_back_window):
-        a = data.iloc[i:(i+look_back_window), :-2]
+        a = data.iloc[i:(i+look_back_window), :-1]
         data_X.append(a)
         b=data.iloc[i+look_back_window,-1]
         data_Y.append(b)
-    #print("OUT X\n", data_X)
-    print("OUT Y\n", data_Y)
+    print("OUT X Shape\n", data_X)
+    print("OUT Y Shape\n", data_Y)
 
 
     return np.array([np.array(xi) for xi in data_X]), np.array(data_Y)